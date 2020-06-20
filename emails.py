d5caf9bb59f39957993b8bddfb83d69f6c445887
cmogush <cmogush@gmail.com>
2020-06-20 07:51:20 -0400

    Update emails.py to correct script

--- emails.py
+++ emails.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3  # shebang
 
-from reportlab.platypus import SimpleDocTemplate  # used to generate PDFs
-from reportlab.platypus import Paragraph, Spacer, Table, Image  # flowables
-from reportlab.lib.styles import getSampleStyleSheet  # used for document styles
-from reportlab.lib import colors  # used for table styles
+import email.message  # used for constructing emails
+import mimetypes  # used for attachments
+import os.path  # used for os directory management
+import smtplib  # simple mail transfer protocol lib, used for sending emails
 
-def generate(filename, title, additional_info, table_data):  # function to generate PDF
-  styles = getSampleStyleSheet()  # sample dictionary of different styling
-  report = SimpleDocTemplate(filename)  # export directory and filename
-  report_title = Paragraph(title, styles["h1"])  # title style
-  report_info = Paragraph(additional_info, styles["BodyText"])  # body style
-  table_style = [('GRID', (0,0), (-1,-1), 1, colors.black),  # table style
-                ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
-                ('ALIGN', (0,0), (-1,-1), 'CENTER')]
-  report_table = Table(data=table_data, style=table_style, hAlign="LEFT")  # table info
-  empty_line = Spacer(1,20)
-  report.build([report_title, empty_line, report_info, empty_line, report_table])  # generates PDF
+def generate(sender, recipient, subject, body, attachment_path):  # generate the email
+  """Creates an email with an attachment."""
+  # Basic Email formatting
+  message = email.message.EmailMessage()  # create message object
+  message["From"] = sender  # set From to sender
+  message["To"] = recipient  # set To to recipient
+  message["Subject"] = subject  # set Subject to subject
+  message.set_content(body)  # set the message body to body
+
+  # Process the attachment and add it to the email
+  attachment_filename = os.path.basename(attachment_path)  # set attachment file
+  mime_type, _ = mimetypes.guess_type(attachment_path)  # automatically guess the mimetype
+  mime_type, mime_subtype = mime_type.split('/', 1)  # split and set the mimetype / subtype
+
+  with open(attachment_path, 'rb') as ap:  # open the attachment
+    message.add_attachment(ap.read(),  # read in the attachment
+                          maintype=mime_type,   # set the mimetype
+                          subtype=mime_subtype,   # set the mime subtype
+                          filename=attachment_filename)  # give the attachment filename
+
+  return message  # returns the finished message
+
+def send(message):  # send the message
+  """Sends the message to the configured SMTP server."""
+  mail_server = smtplib.SMTP('localhost')   # create the smtp object (assumes server configured)
+  mail_server.send_message(message)  # send the message
+  mail_server.quit()  # close the connection
